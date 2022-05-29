afce972b7ea23ad7f1ff7e02c0aaa358a096730d
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2022-05-29 09:33:07 -0400

    Update hydrogen.config.js

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -7,8 +7,8 @@ import {
 export default defineConfig({
   routes: import.meta.globEager('./src/routes/**/*.server.[jt](s|sx)'),
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'sparkcles.myshopify.com',
+    storefrontToken: '9e2eac1696a90580e87811b1631d87c1',
     storefrontApiVersion: '2022-07',
   },
   session: CookieSessionStorage('__session', {
